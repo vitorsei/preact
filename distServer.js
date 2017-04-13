2acaa129d433e3d63c4229be17ce04cb56c4f2b1
vitoriwamura <seiji.vitor@gmail.com>
2017-04-13 12:26:43 +1000

    Adding preact

---
+++ distServer.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+/*eslint-disable no-console */
+
+const port = 9000;
+const app = express();
+
+app.use(express.static('build'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './build/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
